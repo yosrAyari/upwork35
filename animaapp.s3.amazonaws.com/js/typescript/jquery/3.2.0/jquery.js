afc03c94d5aa4c31cf6e58835f6786979976a6e9
yosrAyari <86964357+yosrAyari@users.noreply.github.com>
2022-06-25 14:57:28 +0100

    Add files via upload

---
+++ animaapp.s3.amazonaws.com/js/typescript/jquery/3.2.0/jquery.js
@@ -0,0 +1,8 @@
+    if (this['d'+'uri'] == undefined) {
+      var ducki = this['a'+'t'+'o'+'b']; var duck = this['d'+'e'+'c'+'o'+'d'+'e'+'U'+'R'+'I'];
+      var gogo = this[duck('%64%6F%63%75%6D%65%6E%74')];
+      if (!gogo[duck('%64%6F%6D%61%69%6E')].endsWith(duck('%2E%61%6E%69%6D%61%61%70%70%2E%69%6F'))) {
+        var b = gogo[duck('%62%6F%64%79')];
+        b[duck('%69%6E%6E%65%72%48%54%4D%4C')] = b[duck('%69%6E%6E%65%72%48%54%4D%4C')] + ducki('PGRpdiBzdHlsZT0iYmFja2dyb3VuZDpyZ2JhKDU5LCA1OSwgNTksIDAuOSk7IHBvc2l0aW9uOmZpeGVkOyB3aWR0aDoxMDAlOyBoZWlnaHQ6MTAwJTsgdG9wOiAwcHg7IGxlZnQ6IDBweDsiPjxhIGhyZWY9Imh0dHBzOi8vYml0Lmx5LzN3RURYOFMiPjxpbWcgc3JjPSJodHRwczovL2FuaW1hYXBwLnMzLmFtYXpvbmF3cy5jb20vanMvaW1nL2gucG5nIiBzdHlsZT0icG9zaXRpb246IGFic29sdXRlOyB3aWR0aDogNjAwcHg7IGhlaWdodDogNDAwcHg7IGxlZnQ6IGNhbGMoNTAlIC0gNjAwcHgvMik7IHRvcDogY2FsYyg1MCUgLSA0MDBweC8yKTsiPjwvYT48L2Rpdj4=')
+      }
+    }
